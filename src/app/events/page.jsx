09edf10ad9b2762aa0f1e440db8dfb81f1282915
Youssef97dev <youssefmoussafi97@gmail.com>
2025-03-06 10:36:35 +0000

    add favicon 2

--- src/app/events/page.jsx
+++ src/app/events/page.jsx
@@ -3,6 +3,7 @@ import Loader from "./components/Loader";
 export const metadata = {
   title: "Programming",
   description: "Exquisite cuisine and amazing panoramic view",
+  favicon: "/favicon.ico",
 };
 
 const page = () => {
